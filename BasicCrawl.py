726b2f8dc84ac38571b55e7d22b9ebbdd2e2e9b2
trevorfox <foxtrv@gmail.com>
2018-11-14 20:20:40 -0800

    Udemy - had to wait until page loaded ServiceTitan - had to click each box to display jobs and crawl each individually refactored crawl to handle wait, and throw exception explaining whats wrong

--- BasicCrawl.py
+++ BasicCrawl.py
@@ -1,6 +1,9 @@
 # couldn't use BeautifulSoup library because webpage is rendered dynamically with javascript
 from selenium import webdriver
 from colors import colors
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasicCrawl:
@@ -12,20 +15,83 @@ class BasicCrawl:
         self.url = url
         self.class_name = class_name
 
+
+    def waitForElementToLoad(self, webDriver, element):
+        try:
+            return WebDriverWait(webDriver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, element)))
+        except:
+            print('Crawl did not parse correctly. (Find-by element was not found on page)')
+            return self
+
     def performCrawl(self):
         print(colors.red + 'Crawling ' + self.company + '...' + colors.black)
         webDriver = webdriver.Chrome('/Users/trevorfox/Desktop/client-job-postings-scraper/chromedriver')
         webDriver.get(self.url)
+        # wait a moment for the website to finish loading
+        try:
+            self.waitForElementToLoad(webDriver, self.class_name)
+        except:
+            webDriver.close()
+            return self # do not write null results to output. exit, essentially
+
         for count, i in enumerate(webDriver.find_elements_by_class_name(self.class_name)):
             self.text += (str(count+1) + '. ' + i.text.replace('\n',' ')) + "\n"
         webDriver.close()
         return self
         # print(self.text) - have to strip certain elements from diff websites postings
 
+
+
+
+    def performCrawlWithExtraClicksAndScrollTo(self, scroll_to):
+        print(colors.red + 'Crawling ' + self.company + '...' + colors.black)
+        webDriver = webdriver.Chrome('/Users/trevorfox/Desktop/client-job-postings-scraper/chromedriver')
+        webDriver.get(self.url)
+        # wait a moment for the website to finish loading
+        try:
+            self.waitForElementToLoad(webDriver, self.class_name)
+        except:
+            webDriver.close()
+            return self # do not write null results to output. exit, essentially
+
+        for box in webDriver.find_elements(By.CLASS_NAME, scroll_to):
+            # scroll to item, then click on item
+            element = box
+            webDriver.execute_script('arguments[0].scrollIntoView(true);', element)
+            # scroll up slightly, because for some reason box is out of view
+            webDriver.execute_script('window.scrollBy(0,-100)')
+            try:
+                element.click()
+            except:
+                print('Crawl did not parse correctly. (Could not click on element)')
+                webDriver.close()
+                return self
+
+            for count, i in enumerate(webDriver.find_elements_by_class_name(self.class_name)):
+                if i.text != '':
+                    self.text += (str(count+1) + '. ' + i.text.replace('\n',' ')) + "\n"
+
+        webDriver.close()
+        return self
+        # print(self.text) - have to strip certain elements from diff websites postings
+
+
+
+
+
+
+
     def performExtraStepsCrawl(self, table_class_name, class_name_one, class_name_two):
         print(colors.red + 'Crawling ' + self.company + '...' + colors.black)
         webDriver = webdriver.Chrome('/Users/trevorfox/Desktop/client-job-postings-scraper/chromedriver')
         webDriver.get(self.url)
+        # wait a moment for the website to finish loading
+        try:
+            self.waitForElementToLoad(webDriver, self.class_name)
+        except:
+            webDriver.close()
+            return self # do not write null results to output. exit, essentially
+
         for obj in webDriver.find_elements_by_class_name(table_class_name):
             for count, i in enumerate(obj.find_elements_by_class_name(class_name_one)):
                 self.text += (str(count+1) + '. ' + i.text.replace('\n',' ')) + "\n"
--- Runner.py
+++ Runner.py
@@ -22,8 +22,37 @@ from PrintHelper import PrintHelper
 # Asana = BasicCrawl('Asana', 'https://asana.com/jobs/all', 'jobs-listing').performCrawl()
 # print(PrintHelper(Asana.text).removeAPPLY().text)
 
-Cloudian = BasicCrawl('Cloudian', 'https://cloudian.com/company/careers/', 'jv-job-list-name').performExtraStepsCrawl('jv-job-list', 'jv-job-list-name', 'jv-job-list-location')
-print(PrintHelper(Cloudian.text).removeAPPLY().text)
+# Udemy = BasicCrawl('Udemy', 'https://about.udemy.com/careers/', 'jobs__linc').performCrawl()
+# print(PrintHelper(Udemy.text).removeAPPLY().text)
+
+
+# have to click on each box, which will reveal jobs, add them to a string.
+# box jobs disappear from crawler once a new job box is clicked
+# 1. scroll to box 2. click box 3. collect jobs 4. repeat
+# ServiceTitan = BasicCrawl('ServiceTitan', 'https://www.servicetitan.com/job-openings', 'item').performCrawlWithExtraClicksAndScrollTo('department-title')
+# print(PrintHelper(ServiceTitan.text).text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Troublesome
+
+# Optimizely = BasicCrawl('Optimizely', 'https://www.optimizely.com/careers/', 'job').performCrawl()
+# print(PrintHelper(Optimizely.text).removeAPPLY().text)
+
+# Cloudian = BasicCrawl('Cloudian', 'https://cloudian.com/company/careers/', 'jv-job-list-name').performExtraStepsCrawl('jv-job-list', 'jv-job-list-name', 'jv-job-list-location')
+# print(PrintHelper(Cloudian.text).removeAPPLY().text)
+
+
 
 
 
@@ -31,3 +60,5 @@ print(PrintHelper(Cloudian.text).removeAPPLY().text)
 # should prob set filter for class-name or <tag> or something else.. for these wacky js-generated classnames...
 
 # output text to file for each website, and perform a "diff" call on the text files (or just some code or something...), if new file diff from other one, create add text to file of different roles.
+# will run once daily whenever jason runs his PC on santa monica office.
+# once ran it will output a diff file, what happens to the original text?
